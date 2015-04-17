ce6e2152d2f859d6e5c23fef82d5d4bc7015afde
Tom White <tom@cloudera.com>
2015-04-17 09:54:15 +0100

    [EGGO-30] Generate flattened, partitioned data.

--- eggo/dag.py
+++ eggo/dag.py
@@ -339,6 +339,40 @@ class ADAMPartitionTask(Task):
                                           edition=self.edition))
 
 
+class ADAMFlattenPartitionTask(Task):
+
+    adam_command = Parameter()
+    allowed_file_formats = Parameter()
+    source_edition = 'flat'
+    edition = 'flat_locuspart'
+
+    def requires(self):
+        return ADAMBasicTask(adam_command=self.adam_command,
+                             allowed_file_formats=self.allowed_file_formats)
+
+    def run(self):
+        adam_cmd = ('{adam_home}/bin/adam-submit --master {spark_master_url} partition'
+                    ' -partition_strategy_file {partition_strategy_file}'
+                    ' {source} {target}').format(
+            adam_home=os.environ['ADAM_HOME'],
+            spark_master_url=os.environ['SPARK_MASTER_URL'],
+            partition_strategy_file='genotypes-flat-partition-strategy.json',
+            source=target_s3n_url(ToastConfig().config['name'],
+                                  edition=self.source_edition),
+            target=target_s3n_url(ToastConfig().config['name'],
+                                  edition=self.edition))
+        p = Popen(adam_cmd, shell=True)
+        p.wait()
+
+        if p.returncode == 0:
+            create_SUCCESS_file(target_s3_url(ToastConfig().config['name'],
+                                              edition=self.edition))
+
+    def output(self):
+        return S3FlagTarget(target_s3_url(ToastConfig().config['name'],
+                                          edition=self.edition))
+
+
 class VCF2ADAMTask(Task):
 
     def requires(self):
@@ -347,7 +381,9 @@ class VCF2ADAMTask(Task):
         flat = ADAMFlattenTask(adam_command='vcf2adam',
                                allowed_file_formats=['vcf'])
         locuspart = ADAMPartitionTask(adam_command='vcf2adam',
-                               allowed_file_formats=['vcf'])
+                                      allowed_file_formats=['vcf'])
+        flat_locuspart = ADAMFlattenPartitionTask(adam_command='vcf2adam',
+                                                  allowed_file_formats=['vcf'])
         dependencies = [basic]
         conf = ToastConfig().config
         editions = conf['editions'] if 'editions' in conf else []
@@ -358,6 +394,8 @@ class VCF2ADAMTask(Task):
                 dependencies.append(flat)
             elif edition == 'locuspart':
                 dependencies.append(locuspart)
+            elif edition == 'flat_locuspart':
+                dependencies.append(flat_locuspart)
         return dependencies
 
     def run(self):
@@ -376,6 +414,8 @@ class BAM2ADAMTask(Task):
                                allowed_file_formats=['sam', 'bam'])
         locuspart = ADAMPartitionTask(adam_command='transform',
                                       allowed_file_formats=['sam', 'bam'])
+        flat_locuspart = ADAMFlattenPartitionTask(adam_command='transform',
+                                                  allowed_file_formats=['sam', 'bam'])
         dependencies = [basic]
         conf = ToastConfig().config
         editions = conf['editions'] if 'editions' in conf else []
@@ -386,5 +426,7 @@ class BAM2ADAMTask(Task):
                 dependencies.append(flat)
             elif edition == 'locuspart':
                 dependencies.append(locuspart)
+            elif edition == 'flat_locuspart':
+                dependencies.append(flat_locuspart)
         return dependencies
 
